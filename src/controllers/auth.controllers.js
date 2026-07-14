24b3894a96c32ed8cf99fe29c17993a1e0baabd6
harshpatil1999 <patilh652@gmail.com>
2026-07-15 03:14:08 +0530

    feat(auth):develop an API for user registration

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,35 @@
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+/**
+ * - Controller to register a user
+ * - POST /api/v1/auth/register
+ */
+
+const registerUserController = async (req, res) => {
+  const { email, name, password } = req.body;
+  const doesUserExist = await User.findOne({ email: email });
+  if (doesUserExist) {
+    return res.status(422).json({
+      message: "User already exists!",
+      status: "failed",
+    });
+  }
+  const newUser = await User.create({ email, name, password });
+  const token = await jwt.sign(
+    { userId: newUser._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" },
+  );
+  res.cookies("token", token);
+  return res.status(201).json({
+    user: {
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+    },
+    token,
+  });
+};
+
+module.exports = { registerUserController };
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const authControllers = require("../controllers/auth.controllers");
+
+const router = express.Router();
+
+router.post("/register", authControllers.registerUserController);
+
+module.exports = router;
